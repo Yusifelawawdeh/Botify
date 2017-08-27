5c0497de14a395a9d42a4dddc58e813f77c7d1e9
YusifE <yusifelawawdeh@gmail.com>
2017-08-27 17:26:27 -0500

    basic scraper

--- Botify/Program.cs
+++ Botify/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace Botify
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //var options = new ChromeOptions();
+            //options.AddArguments("--disable-gpu");
+
+            var chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("https://reddit.com");
+
+            var titles = chromeDriver.FindElementsByClassName("title");
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title.Text);
+            }
+
         }
     }
 }
